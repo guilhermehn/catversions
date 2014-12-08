e5ad734af31066b66e9314718dd2c77053ecbddc
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2014-12-08 19:05:34 -0200

    coding style

--- index.js
+++ index.js
@@ -2,58 +2,55 @@
  * Dependencies
  */
 
-var fs = require('fs')
-  , async = require('async')
-  , lib = require('./lib')
+var fs = require('fs');
+var async = require('async');
+var lib = require('./lib');
 
 /**
  * Main function
  */
 
 function catversions (root, cb) {
-
   // Read the root folder
   fs.readdir(root, function (err, data) {
-
     // Pass error to callback
-    if (err) cb(err, null)
+    if (err) {
+      cb(err, null);
+    }
 
     // Add path delimiter if there's none
     if (root.indexOf(/\/$/) === -1) {
-      root += '/'
+      root += '/';
     }
 
     // Filter for directories
     async.filter(data, lib.isDirectory.bind(this, root), function (folders) {
-
       // Filter for directories with version file inside
       async.filter(folders, lib.hasVersionFile.bind(this, root), function (projectFolders) {
-
         // Read version files
         async.map(projectFolders, lib.attachVersion.bind(this, root), function (err, result) {
-
           // Pass error to callback
           if (err) {
-            cb(err, null)
-            return
+            cb(err, null);
+            return;
           }
 
           // `Folder -> Version` hash
-          var versions = {}
+          var versions = {};
 
           result.forEach(function (file) {
-            versions[file[0]] = file[1]
-          })
+            versions[file[0]] = file[1];
+          });
 
-          cb(null, versions)
-        })
-      })
-    })
-  })
+          cb(null, versions);
+        });
+      });
+    });
+  });
 }
 
 /**
  * Exports
  */
 
-module.exports = catversions
+module.exports = catversions;
